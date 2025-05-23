1cb7e8ba6d7044f346a89b4cbf8997bee3240139
최효재 <hyojae5945@gmail.com>
2025-05-23 17:49:19 +0900

    feat : 상품 등록 기능 추가

--- cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -3,21 +3,52 @@ package sample.cafekiosk.spring.api.service.product;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 
+/**
+ * readOnly = true : 읽기 전용
+ * CRUD에서 CUD 동작 X / only READ
+ * JPA : CUD 스냅샷 저장, 변경감지 X (성능 향상)
+ *
+ * CQRS - Command / Query -> Read 가 압도적으로 많이 불림
+ */
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ProductService {
 
   private final ProductRepository productRepository;
 
   public List<ProductResponse> getSellingProducts() {
-    List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
+    List<Product> products = productRepository.findAllBySellingStatusIn(
+        ProductSellingStatus.forDisplay());
     return products.stream()
         .map(ProductResponse::of)
         .toList();
+  }
+
+  // 동시성 이슈
+  @Transactional
+  public ProductResponse createProduct(ProductCreateRequest request) {
+    String nextProductNumber = createNextProductNumber();
+
+    Product product = request.toEntity(nextProductNumber);
+    Product savedProduct = productRepository.save(product);
+
+    return ProductResponse.of(savedProduct);
+  }
+
+  private String createNextProductNumber() {
+    String latestProductNumber = productRepository.findLatestProductNumber();
+    if (latestProductNumber == null) {
+      return "001";
+    }
+    int nextProductNumber = Integer.parseInt(latestProductNumber) + 1;
+    return String.format("%03d", nextProductNumber);
   }
 }
---
+++ cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/product/ProductServiceTest.java
@@ -0,0 +1,107 @@
+package sample.cafekiosk.spring.api.service.product;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
+import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
+
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
+import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductRepository;
+import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+import sample.cafekiosk.spring.domain.product.ProductType;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ProductServiceTest {
+
+  @Autowired
+  private ProductService productService;
+
+  @Autowired
+  private ProductRepository productRepository;
+
+  @AfterEach
+  void tearDown() {
+    productRepository.deleteAllInBatch();
+  }
+
+  @DisplayName("신규 상품을 등록할 때, 상품이 하나도 없는 경우 신규 상품을 등록하면 상품번호는 001이다.")
+  @Test
+  void createProductWhenProductsIsEmpty() {
+    // given
+    ProductCreateRequest request = ProductCreateRequest.builder()
+        .name("카푸치노")
+        .type(HANDMADE)
+        .sellingStatus(SELLING)
+        .price(5000)
+        .build();
+
+    // when
+    ProductResponse productResponse = productService.createProduct(request);
+
+    // then
+    assertThat(productResponse)
+        .extracting("productNumber", "name", "productType", "sellingStatus", "price")
+        .containsExactly("001", "카푸치노", HANDMADE, SELLING, 5000);
+
+    List<Product> products = productRepository.findAll();
+    assertThat(products)
+        .hasSize(1)
+        .extracting("productNumber", "name", "type", "sellingStatus", "price")
+        .containsExactlyInAnyOrder(
+            tuple("001", "카푸치노", HANDMADE, SELLING, 5000)
+        );
+  }
+
+  @DisplayName("신규 상품을 등록한다. 상품번호는 가장 최근 상품의 번호에서 1 증가한 값이다.")
+  @Test
+  void createProduct() {
+    // given
+    Product product1 = createProduct("001", HANDMADE, SELLING, "아메리카노", 4000);
+    productRepository.save(product1);
+
+    ProductCreateRequest request = ProductCreateRequest.builder()
+        .name("카푸치노")
+        .type(HANDMADE)
+        .sellingStatus(SELLING)
+        .price(5000)
+        .build();
+
+    // when
+    ProductResponse productResponse = productService.createProduct(request);
+
+    // then
+    assertThat(productResponse)
+        .extracting("productNumber", "name", "productType", "sellingStatus", "price")
+        .containsExactly("002", "카푸치노", HANDMADE, SELLING, 5000);
+
+    List<Product> products = productRepository.findAll();
+    assertThat(products)
+        .hasSize(2)
+        .extracting("productNumber", "name", "type", "sellingStatus", "price")
+        .containsExactlyInAnyOrder(
+            tuple("001", "아메리카노", HANDMADE, SELLING, 4000),
+            tuple("002", "카푸치노", HANDMADE, SELLING, 5000)
+        );
+  }
+
+  private Product createProduct(String productNumber, ProductType type,
+      ProductSellingStatus sellingStatus, String name, int price) {
+    return Product.builder()
+        .productNumber(productNumber)
+        .type(type)
+        .sellingStatus(sellingStatus)
+        .name(name)
+        .price(price)
+        .build();
+  }
+}
